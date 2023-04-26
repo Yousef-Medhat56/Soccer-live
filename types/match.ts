6fa956ee6809a18493cf1f234f9a497910b7d5bf
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-04-26 11:21:38 +0200

    feat: create "Match" type

---
+++ types/match.ts
@@ -0,0 +1,20 @@
+export type Match = {
+  matchId: string;
+  matchStatus: string;
+  matchTime?: string;
+  homeName: string;
+  homeImg?: string;
+  homeUrl?: string;
+  homeScore?: string;
+  awayName: string;
+  awayImg?: string;
+  awayUrl?: string;
+  awayScore?: string;
+};
+
+export type LeagueMatches = {
+  leagueId: string;
+  leagueName: string;
+  leagueImg?: string;
+  matches: Match[];
+};
