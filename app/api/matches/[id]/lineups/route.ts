d03466d3d464d9253f8931bf7573b55cca22210c
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-05-03 19:46:58 +0300

    feat: add enpoint(GET /matches/{id}/lineups)
    body: the endpoint returns teams lineups (main formation and substitutes)

---
+++ app/api/matches/[id]/lineups/route.ts
@@ -0,0 +1,50 @@
+import { createTeamFormation, getSubsitutes } from "./utils";
+
+export async function GET(
+  req: Request,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) {
+  const { id } = params;
+  const url = `https://www.btolat.com/matches/LinupGet/${id}`;
+  //fetch data
+  const response = await fetch(url);
+  const data = await response.json();
+
+  //check if the response is successful
+  if (response.status==200) {
+    //home team formation
+    const homeTeam = createTeamFormation(
+      data.localteam.formation,
+      data.localteam.lstlinup
+    );
+
+    //add subsitute players
+    const homeSubstitutes = await getSubsitutes(id, true);
+    homeTeam.substitutes = homeSubstitutes;
+
+    //away team formation
+    const awayTeam = createTeamFormation(
+      data.vistorteam.formation,
+      data.vistorteam.lstlinup
+    );
+    //add subsitute players
+    const awaySubstitutes = await getSubsitutes(id, false);
+    awayTeam.substitutes = awaySubstitutes;
+
+    return new Response(JSON.stringify({ home: homeTeam, away: awayTeam }));
+  } else {
+    return new Response(
+      JSON.stringify({
+        data: {
+          matchId: id,
+          message: `Match lineups are not available.`,
+        },
+      }),
+      { status: 404 }
+    );
+  }
+}
