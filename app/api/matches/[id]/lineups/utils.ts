871a914aaa83c31a3c09678f40572e80105e8c37
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-05-03 19:15:17 +0300

    feat: create team formation of main players

---
+++ app/api/matches/[id]/lineups/utils.ts
@@ -0,0 +1,68 @@
+import { PlayerInLineup, TeamLineup } from "@/types/lineup";
+
+//players positions by number of formation lines
+const positionsByFormLines = {
+  3: ["D", "M", "F"], //ex: 4-3-3
+  4: ["D", "DM", "AM", "F"], //ex: 4-2-3-1
+  5: ["D", "DM", "M", "AM", "F"], //ex: 4-1-2-1-2
+};
+
+type Player = {
+  Name: string;
+  Number: number;
+  formation_pos: number;
+};
+
+//sort players by their position number ascendingly
+export const sortPlayersByPos = (playersList: Player[]) => {
+  playersList.sort((a, b) => a.formation_pos - b.formation_pos);
+  return playersList;
+};
+
+export const createTeamFormation = (
+  teamForm: string, //team formation, example: 4-3-3
+  playersList: Player[]
+) => {
+  const orderedPlayersList = sortPlayersByPos(playersList);
+
+  //split the team formation and convert it to numbers
+  const teamFormArr = teamForm.split("-").map(Number);
+
+  //@ts-ignore
+  const teamFormLines = positionsByFormLines[teamFormArr.length];
+
+  //goalkeeper data
+  const goalkeeper: PlayerInLineup = {
+    name: orderedPlayersList[0].Name,
+    shirtNumber: orderedPlayersList[0].Number,
+    positionNumber: orderedPlayersList[0].formation_pos,
+  };
+
+  const team: TeamLineup = {
+    formation: teamForm,
+    mainPlayers: {
+      GK: goalkeeper,
+      D: [],
+      DM: [],
+      M: [],
+      AM: [],
+      F: [],
+    },
+  };
+
+  let playerIndex = 1;
+  teamFormArr.forEach((x, index) => {
+    for (let i = 0; i < x; i++) {
+      const newPlayer = {
+        name: orderedPlayersList[playerIndex].Name,
+        shirtNumber: orderedPlayersList[playerIndex].Number,
+        positionNumber: orderedPlayersList[playerIndex].formation_pos,
+      };
+      //@ts-ignore
+      team.mainPlayers[teamFormLines[index]].push(newPlayer);
+      playerIndex++;
+    }
+  });
+
+  return team;
+};
