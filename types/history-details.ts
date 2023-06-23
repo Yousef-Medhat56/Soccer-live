e7dc8b183e21eb672d74898df0482801f3779270
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-06-23 16:43:23 +0300

    feat: define new type (MatchHistoryDetails)
    body: The type holds the data of the history matches of the 2 teams

---
+++ types/history-details.ts
@@ -0,0 +1,26 @@
+type TeamInMatch = {
+  name: string;
+  teamlUrl: string;
+  teamImg: string;
+  teamGoals: number;
+};
+
+type Match = {
+  date: string;
+  league: string;
+  homeTeam: TeamInMatch;
+  awayTeam: TeamInMatch;
+};
+
+export type MatchHistoryDetails = {
+  //face to face history
+  f2fHistory: {
+    homeWins: number;
+    draw: number;
+    awayWins: number;
+  };
+  f2fResults?: Match[]; //face to face results
+  f2fBigWins?: Match[]; //face to face big wins
+  homeLastMatches?: Match[];
+  awayLastMatches?: Match[];
+};
