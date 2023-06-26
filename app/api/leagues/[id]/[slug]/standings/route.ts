f220e3dd807602d685a17abd68e8b0ac390f14db
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-06-26 21:47:14 +0300

    feat: finish /league/{id}/standings endpoint

---
+++ app/api/leagues/[id]/[slug]/standings/route.ts
@@ -0,0 +1,37 @@
+import * as cheerio from "cheerio";
+import { getStandings } from "./utils";
+
+export async function GET(
+  req: Request,
+  {
+    params,
+  }: {
+    params: { id: number; slug: string };
+  }
+) {
+  const { id, slug } = params;
+  const url = `https://www.btolat.com/league/standings/${id}/${slug}`;
+  //fetch data
+  let response = await fetch(url);
+
+  const data = await response.text();
+
+  //load data to cheerio
+  const $ = cheerio.load(data);
+
+  //check if the league table exists
+  if ($(".leagueTable").length) {
+    const leagueStandings = getStandings($);
+    return new Response(JSON.stringify({ data: { groups: leagueStandings } }));
+  } else {
+    return new Response(
+      JSON.stringify({
+        data: {
+          league: `${id}/${slug}`,
+          message: `League standings are not available.`,
+        },
+      }),
+      { status: 404 }
+    );
+  }
+}
