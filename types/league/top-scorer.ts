65513a8fba49ad2eca1ab1be3ae8bbc3c7261a1f
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-07-01 17:47:18 +0300

    feat: create types related to top scorers data

---
+++ types/league/top-scorer.ts
@@ -0,0 +1,13 @@
+export type TopScorer = {
+  playerData: {
+    name: String;
+    url?: String;
+    img?: String;
+  };
+  clubData: {
+    name: String;
+    url?: String;
+    img?: String;
+  };
+  goalsNum: Number;
+};
