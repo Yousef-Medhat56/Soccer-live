1e9b7a649b83dc39bbd17a7a738984a75acf0a1e
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-08-08 03:44:11 +0300

    feat(frontend): add /leagues/[id]/[slug]/standings

---
+++ app/components/leagues/container/league-standings.tsx
@@ -0,0 +1,30 @@
+import StandingsTable from "@/app/components/tables/standings-table";
+import { GroupStandings } from "@/types/league/standings";
+
+export default async function LeagueStandingsComp({
+  id,
+  slug,
+}: {
+  id: string;
+  slug: string;
+}) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_URL}/api/leagues/${id}/${slug}/standings`
+  );
+
+  const {
+    data: { groups },
+  }: { data: { groups: GroupStandings[] } } = await response.json();
+
+  return (
+    <>
+      {groups ? (
+        groups.map((group, index) => (
+          <StandingsTable key={index} group={group} />
+        ))
+      ) : (
+        <h2 className="text-label pt-4 md:pt-6 text-center">لا يوجد ترتيب</h2>
+      )}
+    </>
+  );
+}
