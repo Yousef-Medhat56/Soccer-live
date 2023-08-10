364d61e23e1d26234726fb5fac030c923e46dd80
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-08-10 20:17:48 +0300

    feat(api): add /club/[id]/[slug] endpoint

--- app/api/club/[id]/[slug]/route.ts
+++ app/api/club/[id]/[slug]/route.ts
@@ -23,48 +23,21 @@ export async function GET(
 
   //check if the response is successful
   if (response.status === 200) {
-    //club details object
-    const clubDetails: ClubDetails = {
-      name: "",
-      participatingLeagues: [],
-    };
-
     //club name
-    clubDetails.name = $(".details h2").text();
+    const clubName = $(".details h2").text();
 
-    //loop through club details
-    $(".details h3").each(function () {
-      const key = $(this).clone().children().remove().end().text();
+    //club logo
+    const clubImg = $(".leagueTitle img").attr("src");
 
-      //club country
-      if (key === "البلد ") clubDetails.country = $(this).find("span").text();
-      //club foundation year
-      else if (key === "تاريخ التأسيس ")
-        clubDetails.foundationYear = +$(this).find("span").text();
-      //club manager
-      else if (key === "المدير الفني ")
-        clubDetails.manager = $(this).find("span").text();
-      //club stadium
-      else if (key === "الملعب ")
-        clubDetails.stadium = $(this).find("span").text();
-    });
-
-    //loop through the leagues that the club participate in
-    $("h3 a").each(function () {
-      const league: LeagueLink = {
-        name: $(this).text(),
-        url: removePartfromStr($(this).attr("href")!, "/league/"),
-      };
-      clubDetails.participatingLeagues.push(league);
-    });
-
-    return new Response(JSON.stringify({ data: { clubDetails } }));
+    return new Response(
+      JSON.stringify({ data: { club: { name: clubName, img: clubImg } } })
+    );
   } else {
     return new Response(
       JSON.stringify({
         data: {
           club: `${id}/${slug}`,
-          message: `Club data is not available.`,
+          message: `Club ddoesn't exist`,
         },
       }),
       { status: 404 }
